72557209d93d4f827447f5cf209bafd35ed1354a
Francis Omondi <francisomondi17@gmail.com>
2026-01-04 06:07:05 +0300

    create product image upload

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,8 +5,8 @@ import FeaturedProducts from "../components/FeaturedProducts";
 import Footer from "../components/Footer";
 
 const categories = [
-	{ href: "/jeans", name: "Jeans", imageUrl: "/jeans.jpg" },
-	{ href: "/t-shirts", name: "T-shirts", imageUrl: "/tshirts.jpg" },
+	{ href: "/jeans", name: "Pants", imageUrl: "/jeans.jpg" },
+	{ href: "/t-shirts", name: "Shirts", imageUrl: "/tshirts.jpg" },
 	{ href: "/shoes", name: "Shoes", imageUrl: "/shoes.jpg" },
 	{ href: "/glasses", name: "Glasses", imageUrl: "/glasses.png" },
 	{ href: "/jackets", name: "Jackets", imageUrl: "/jackets.jpg" },
